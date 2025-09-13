f04712716699971862aa5ee766fc9e44c1679ebd
Sergio Mondragon <smondragnsilva@byupathway.edu>
2025-09-12 19:47:07 -0500

    W02 Assignment completed

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,67 @@
+const path = "scripts/members.json";
+const businessSection = document.getElementById("business-section");
+const headerList = document.getElementById("header-list");
+
+async function getMembersData(){
+    const response = await fetch(path);
+    const data = await response.json();
+    displayMember(data);
+}
+
+const displayMember = (members) => {
+    members.forEach(member => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const cardHeader = document.createElement("div");
+        cardHeader.classList.add("card-header");
+        const businessName = document.createElement("h2");
+        const tagLine = document.createElement("p");
+        businessName.textContent = member.name;
+        tagLine.textContent = member.companyLine;
+        cardHeader.appendChild(businessName);
+        cardHeader.appendChild(tagLine);
+
+        const cardContent = document.createElement("div");
+        cardContent.classList.add("card-content");
+        const businessImage = document.createElement("img");
+        businessImage.setAttribute("src", member.image);
+        businessImage.setAttribute("loading", "lazy");
+        businessImage.setAttribute("width", "120");
+        businessImage.setAttribute("alt", `Profile of ${member.name}`);
+        const ul = document.createElement("ul");
+        const emailLi = document.createElement("li");
+        emailLi.innerHTML = `<span>EMAIL:</span> ${member.email}`;
+        const phoneLi = document.createElement("li");
+        phoneLi.innerHTML = `<span>PHONE:</span> ${member.phone}`;
+        const urlLi = document.createElement("li");
+        urlLi.innerHTML = `<span>URL:</span> ${member.website}`;
+        ul.appendChild(emailLi);        
+        ul.appendChild(phoneLi);
+        ul.appendChild(urlLi);
+        cardContent.appendChild(businessImage);
+        cardContent.appendChild(ul);
+
+        // Appending child to the card element
+        card.appendChild(cardHeader);
+        card.appendChild(cardContent);
+
+        // Business section
+        businessSection.appendChild(card);
+    });
+}
+
+const gridButton = document.querySelector("#grid-view");
+gridButton.addEventListener('click', () => {
+    businessSection.classList.remove("list");
+    headerList.classList.remove("show");
+})
+
+const listButton = document.querySelector("#list-view");
+listButton.addEventListener('click', () => {
+    businessSection.classList.add("list");
+    headerList.classList.add("show");
+})
+
+
+getMembersData();
